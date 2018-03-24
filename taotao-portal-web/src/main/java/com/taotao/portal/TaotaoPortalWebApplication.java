cec2bc095b0f93afb65786c95ae7fccb21da97dd
dujuhui <362992069@qq.com>
2018-03-24 22:57:40 +0800

    配置文件

---
+++ taotao-portal-web/src/main/java/com/taotao/portal/TaotaoPortalWebApplication.java
@@ -0,0 +1,12 @@
+package com.taotao.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaotaoPortalWebApplication.class, args);
+	}
+}
