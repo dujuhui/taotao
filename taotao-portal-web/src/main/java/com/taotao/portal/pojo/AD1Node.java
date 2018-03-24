37e9b3f03dbdb5e83692e0568d7be84cc3f1459a
dujuhui <362992069@qq.com>
2018-03-24 22:42:46 +0800

    前台系统

---
+++ taotao-portal-web/src/main/java/com/taotao/portal/pojo/AD1Node.java
@@ -0,0 +1,18 @@
+package com.taotao.portal.pojo;
+
+import lombok.Data;
+
+@Data
+public class AD1Node {
+
+	private String srcB;
+	private Integer height;
+	private String alt;
+	private Integer width;
+	private String src;
+	private Integer widthB;
+	private String href;
+	private Integer heightB;
+
+	
+}
