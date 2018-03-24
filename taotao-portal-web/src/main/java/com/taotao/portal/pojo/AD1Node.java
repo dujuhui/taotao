b29fd6eeda842e0606d9cb6cf0415a97523fb7dc
dujuhui <362992069@qq.com>
2018-03-24 23:35:46 +0800

    轮播图展示

--- taotao-portal-web/src/main/java/com/taotao/portal/pojo/AD1Node.java
+++ taotao-portal-web/src/main/java/com/taotao/portal/pojo/AD1Node.java
@@ -1,8 +1,6 @@
 package com.taotao.portal.pojo;
 
-import lombok.Data;
 
-@Data
 public class AD1Node {
 
 	private String srcB;
@@ -14,5 +12,67 @@ public class AD1Node {
 	private String href;
 	private Integer heightB;
 
-	
+	public String getSrcB() {
+		return srcB;
+	}
+
+	public void setSrcB(String srcB) {
+		this.srcB = srcB;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public String getAlt() {
+		return alt;
+	}
+
+	public void setAlt(String alt) {
+		this.alt = alt;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	public Integer getWidthB() {
+		return widthB;
+	}
+
+	public void setWidthB(Integer widthB) {
+		this.widthB = widthB;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public Integer getHeightB() {
+		return heightB;
+	}
+
+	public void setHeightB(Integer heightB) {
+		this.heightB = heightB;
+	}
 }
